87b55caa00c87409f568b291906fb0115a1e395e
aa-aitmahfoud <ahmedaitm26@gmail.com>
2026-08-08 16:36:31 +0100

    Add variables.c: pracice with variables

---
+++ variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int age = 20;
+    float height = 1.75;
+    char initial = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Initial: %c\n", initial);
+    return 0;
+}
