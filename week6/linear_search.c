6fc68a9f008c5c7a796da413dabf12a333b69316
aa-aitmahfoud <ahmedaitm26@gmail.com>
2026-08-11 00:35:30 +0100

    Add linear_search.c: simple linear search function in C

---
+++ week6/linear_search.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int linear_search(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+
+}
+
+int main () {
+
+    int numbers[5] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int target;
+
+    printf("Enter a number to search for: ");
+    scanf("%d", &target);
+    int index = linear_search(numbers, size, target);
+    if (index == -1) {
+        printf("Sorry the value %d not found\n", target);
+    } else {
+        printf("The value %d is found at index: %d\n", target, index);
+    }
+
+    return 0;
+}
